9cf92a4fe0e6c87cddda4a11ee1c0d1c7234572f
송민섭 <minsub1031@gmail.com>
2022-06-26 21:56:45 +0900

    finish QR scanner!!

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
-import time
-
 import cv2
-import pyzbar.pyzbar as pyzbar
-from playsound import playsound
 import requests
 
 cap = cv2.VideoCapture(0)
+d = cv2.QRCodeDetector()
 
 if not cap.isOpened(): # 카메라가 잘 열리지 않은 경우
     exit()
+
+SCALE = 10 # 크기
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+THICKNESS = 30 # 두께
+PASS = 'PASS'
+FAILED = 'FAILED'
+FONT =  cv2.FONT_HERSHEY_SIMPLEX
 
 while True:
     success, frame = cap.read()
 
     if not success:
         break
-
-    for code in pyzbar.decode(frame):
-        my_code = code.data.decode('utf-8')
-
-        print('인식 성공: ' + my_code)
-        playsound("sound.mp3")
-        time.sleep(1)
+    code, _, _ = d.detectAndDecode(frame)
+    if code:
+        url = f'http://127.0.0.1:5000/image/cam/{code}'
+        headers = {'camera_request': r"wY?a&KtTF;\S6so"}
+        try:
+            res = requests.get(url, headers=headers).json()
+            if res['data'] == "success":
+                cv2.putText(frame, PASS, (250, 450),FONT, SCALE, GREEN, THICKNESS)
+            else:
+                cv2.putText(frame, FAILED, (150, 450), FONT, SCALE, RED, THICKNESS)
+        except:
+            continue
 
     cv2.imshow('CAM', frame)
-    if cv2.waitKey(1) == ord('q'):  # 사용자가 q를 입력하면
+    if cv2.waitKey(1)&0xFF == 27:  # 사용자가 q를 입력하면
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-code = None
-url = f'http://127.0.0.1:5000/image/cam/{code}'
-headers = {'camera_request' : r"wY?a&KtTF;\S6so"}
-
-res = requests.get(url, headers=headers)
